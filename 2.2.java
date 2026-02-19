9bfadd003f258a702468e0bcee2d64d3bc0eecb0
Anish Kumar <ay6704197@gmail.com>
2026-02-19 19:11:47 +0530

    update 2.2.java

---
+++ 2.2.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		int x=856;
+		int y=(int)x/10;
+		System.out.println( "x="+y);
+	}
+}
+/*2. Write a program to print a number stored in a variable without its last digit*/
